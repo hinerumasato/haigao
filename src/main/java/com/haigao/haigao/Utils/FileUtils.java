357537a3ed3af6f44cb94bbdb93d55c0418e4d6b
Thang Loi <thangloitran406@gmail.com>
2023-03-24 21:22:58 +0700

    Fix Warning FileUtils

--- src/main/java/com/haigao/haigao/Utils/FileUtils.java
+++ src/main/java/com/haigao/haigao/Utils/FileUtils.java
@@ -10,16 +10,21 @@ public class FileUtils {
         String path = System.getProperty("user.dir");
         path = path.replaceAll("\\\\", "/");
         path += "/src/main/resources/static/Img/Uploads/";
-        
-        String imagePath = image.getOriginalFilename().replaceAll("\\\\", "/");
+
+        String imagePath = image.getOriginalFilename();
         path += imagePath;
+        path = path.replaceAll("\\\\", "/");
+
         image.transferTo(new File(path));
     }
 
     public static String getRootPath(MultipartFile image) {
         String result = "/img/uploads/";
-        String imagePath = image.getOriginalFilename().replaceAll("\\\\", "/");
+        String imagePath = image.getOriginalFilename();
         result += imagePath;
+
+        result = result.replaceAll("\\\\", "/");
+
         return result;
     }
 }
